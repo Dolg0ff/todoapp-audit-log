17be5b6e8509e4fdebd2157786fc0a5043e1ec13
Yuriy Dolgov <ys.dolgoff@gmail.com>
2025-01-05 16:01:03 +0300

    actions & entities & protobuf

---
+++ internal/repository/audit.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"context"
+
+	audit "github.com/Dolg0ff/todoapp-audit-log/pkg/domain"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Audit struct {
+	db *mongo.Database
+}
+
+func NewAudit(db *mongo.Database) *Audit {
+	return &Audit{
+		db: db,
+	}
+}
+
+func (r *Audit) Insert(ctx context.Context, item audit.LogItem) error {
+	_, err := r.db.Collection("logs").InsertOne(ctx, item)
+
+	return err
+}
